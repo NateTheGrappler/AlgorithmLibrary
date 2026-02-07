67f3b3e450f8f3eb0a70d14cec1b285d65853666
NateTheGrappler <guzinskinatan@gmail.com>
2026-02-06 19:25:08 -0500

    finished up shell sort

---
+++ shellSort/shellSort.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+std::vector<int> shellSort(std::vector<int> arr)
+{
+    int n = arr.size();
+    
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            int temp = arr[i];
+            int j = i;
+
+            while (gap <= j && arr[j - gap] > temp)
+            {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    return arr;
+
+}
---
+++ shellSort/shellSort.js
@@ -0,0 +1,22 @@
+
+function shellSort(arr)
+{
+    let n = arr.length;
+
+    for(let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap/2))
+    {
+        for(let i = gap; i < n; i++)
+        {
+            let temp = arr[i];
+            let j = i;
+
+            while(gap <= j && arr[j-gap] > temp)
+            {
+                arr[j] = arr[j-gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+    }
+    return arr;
+}
