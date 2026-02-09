7fdde6bf9ebb948c721b57daf4d3c05711c8a8a2
NateTheGrappler <guzinskinatan@gmail.com>
2026-02-09 15:57:07 -0500

    finished counting sort

--- countingSort/countingSort.js
+++ countingSort/countingSort.js
@@ -1,6 +1,27 @@
 
 
-function countingSort()
+function countingSort(arr)
 {
+    let n = arr.length;
+    let maxVal = Math.max(...arr)
+    let countArray = new Array(maxVal + 1).fill(0);
 
+    for(let value of arr)
+    {
+        countArray[value] += 1;
+    }
+
+    for(let i = 1; i <= maxVal; i++)
+    {
+        countArray[i] += countArray[i-1];
+    }
+
+    let answerArray = new Array(n)
+    for(let i = n-1; i >= 0; i--)
+    {
+        answerArray[countArray[arr[i]] - 1] = arr[i];
+        countArray[arr[i]] -= 1;
+    }
+
+    return answerArray;
 }
